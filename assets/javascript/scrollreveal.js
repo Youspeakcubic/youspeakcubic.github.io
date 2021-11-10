d2951202233b113a51120ff8a85774a3b5951b28
Anthony De la Torre <75534476+Youspeakcubic@users.noreply.github.com>
2021-11-10 07:51:01 -0600

    Add files via upload
    added scroll reveal library and implemented to elements

---
+++ assets/javascript/scrollreveal.js
@@ -0,0 +1,9 @@
+
+console.log("ScrollReveal Loaded");
+ScrollReveal().reveal('.typeWriterContainer', {delay: 200})
+ScrollReveal().reveal(".landingPara", {delay: 1000 });
+ScrollReveal().reveal(".arrow", {delay: 1000});
+ScrollReveal().reveal('#skillsCaro', {delay: 200, reset:true,duration:2000})
+ScrollReveal().reveal('.slantSection1', {delay: 200, reset:true,duration:2000})
+ScrollReveal().reveal('.slantsection2', {delay: 200, reset:true,duration:2000})
+ScrollReveal().reveal('.contactForm', {delay: 200, reset:true,duration:2000})
